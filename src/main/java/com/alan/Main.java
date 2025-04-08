b1add0e6d14fc33bd7c34d48d1ff0d9879b609f2
1 <codingmark31@gmail.com>
2025-04-08 16:25:46 +0800

    2

---
+++ src/main/java/com/alan/Main.java
@@ -0,0 +1,10 @@
+package com.alan;
+
+public class Main {
+    public static void main(String[] args) {
+       // System.out.println("Hello World");
+     Person p=new Person();
+     p.hello();
+        //new Person().hello();
+    }
+}
---
+++ src/main/java/com/alan/Person.java
@@ -0,0 +1,7 @@
+package com.alan;
+
+public class Person {
+   public void hello(){
+       System.out.println("hello12");
+   }
+}
