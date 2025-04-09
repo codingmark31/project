179c2594bdf36af5ef79f990c330bf8f97d40e09
1 <codingmark31@gmail.com>
2025-04-09 12:20:30 +0800

    2

--- src/main/java/com/alan/Main.java
+++ src/main/java/com/alan/Main.java
@@ -3,13 +3,24 @@ package com.alan;
 public class Main {
     public static void main(String[] args) {
        // System.out.println("Hello World");
-     Person p=new Person();
+        Person p=new Person();
+        Dice d=new Dice();
+
      p.hello();
-     int age = 20;
-     float weight=65.5f;
-     double height=160.8;
-
-
+        p.age=20;
+        p.name="Alan";
+        p.weight=50;
+        p.height = 1.6f;
+        p.print();
+        System.out.println("bmi="+p.bmi());
+        p.age=17;
+        p.name="mary";
+        p.weight=40;
+        p.height = 1.5f;
+        p.print();
+        System.out.println("bmi="+p.bmi());
+        System.out.println("max="+d.max);
+        System.out.println("max="+d.point);
         //new Person().hello();
     }
 }
--- src/main/java/com/alan/Person.java
+++ src/main/java/com/alan/Person.java
@@ -1,7 +1,22 @@
 package com.alan;
-
 public class Person {
+    String name;
+    int age;
+    float weight;
+    float height;
+    public double bmi() {
+        double  bmi = weight / (height*height);
+        return bmi;
+    }
    public void hello(){
-       System.out.println("hello12");
+       System.out.println("hello");
+
    }
+   public void print(){
+        System.out.println("name="+name);
+        System.out.println("age="+age);
+        System.out.println("weight="+weight);
+        System.out.println("height="+height);
+    }
+
 }
